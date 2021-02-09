b718ce922df3a4de30c9b1c424c61e6c872c9084
Laurent Fréval <laurent.freval@outlook.com>
2021-02-09 08:25:01 +0100

    Ajoutez des fichiers projet.

---
+++ test1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Module1
+
+    Sub Main()
+        ' Console.WriteLine("Hello World" + "(from Visual Basic Net)" + " sdfksdfsmdlfksmldfksmldfsmldkfjlsdkfjlsdkjf")
+        ' MsgBox("VB est partout")
+
+        Dim maClio As New Voiture()
+        Console.WriteLine(maClio.lesRoues)
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ test1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("test1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("laurent.freval@outlook.com")>
+<Assembly: AssemblyProduct("test1")>
+<Assembly: AssemblyCopyright("Copyright © laurent.freval@outlook.com 2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("6cd7a5c7-2d8a-4fbf-ae10-258993db8efe")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
